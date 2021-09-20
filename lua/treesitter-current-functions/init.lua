fec33cc16e1c76dc2edc8f5e97e3db69d30d3558
eckon <niklas.meyer95@hotmail.de>
2021-09-20 12:47:40 +0200

    Use named node instead of random ones by index
    After having some problems in different languages and not finding an
    easy way to get a specific named node, I added a function to fetch the
    wanted named node myself.
    
    This allows the plugin to be more stable and less likely to get a node
    that is not the correct one.
    
    Example: Normally the 2nd node is the name of a class, but some
    languages have a prefix (async, static, etc.) in front of the class
    name, meaning that now the correct one would be the 3rd or 4th node,
    which I do not want, because treesitter should handle that not me.

--- lua/treesitter-current-functions/init.lua
+++ lua/treesitter-current-functions/init.lua
@@ -17,10 +17,19 @@ local function get_node_information(node)
   return { line_number, line_content }
 end
 
+local function get_named_node(parent, named)
+  for node, name in parent:iter_children() do
+    if name == named then
+      return node
+    end
+  end
+end
+
 local function get_function_list_of_parent(parent)
   local content = {}
 
   for tsnode in parent:iter_children() do
+
     local is_simple_function =
       tsnode:type() == "function_declaration" or
       tsnode:type() == "function_definition" or
@@ -28,11 +37,14 @@ local function get_function_list_of_parent(parent)
       tsnode:type() == "method_definition" or
       tsnode:type() == "method_declaration"
 
+    -- standard ways of declaring/defining functions
     if is_simple_function then
       local info = get_node_information(tsnode)
       table.insert(content, info)
     end
 
+    -- export might be used to export class/namespace/variables
+    -- which include or are functions in itself
     if tsnode:type() == "export_statement" then
       local info = get_function_list_of_parent(tsnode)
 
@@ -41,11 +53,12 @@ local function get_function_list_of_parent(parent)
       end
     end
 
+    -- a lexical declartion can assign a function to a value
     if tsnode:type() == "lexical_declaration" then
       local child = tsnode:child(1)
 
       if child:type() == "variable_declarator" then
-        local function_node = child:child(2)
+        local function_node = get_named_node(child, "value")
 
         if function_node:type() == "arrow_function" then
           local info = get_node_information(child)
@@ -54,14 +67,15 @@ local function get_function_list_of_parent(parent)
       end
     end
 
-    -- in case more functions might be inside of other structures
+    -- a class might have multiple functions
     if tsnode:type() == "class_declaration" then
-      local class_name_node = tsnode:child(1)
+      local class_name_node = get_named_node(tsnode, "name")
       local class_name = ts_utils.get_node_text(class_name_node)[1]
 
       -- get the class body of the class
       -- this might contain functions (methods)
-      local info = get_function_list_of_parent(tsnode:child(2))
+      local body = get_named_node(tsnode, "body")
+      local info = get_function_list_of_parent(body)
 
       for _, node_information in ipairs(info) do
         -- append class infront of methods
