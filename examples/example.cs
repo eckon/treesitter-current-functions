bdd1fc09d964c58400d1966944de425c113e43ac
eckon <niklas.meyer95@hotmail.de>
2021-09-20 11:25:49 +0200

    Add c# example

---
+++ examples/example.cs
@@ -0,0 +1,17 @@
+namespace Example.Model
+{
+    public class ExampleManager
+    {
+        public ExampleManager()
+        {
+        }
+
+        public async Task<foo> Start(int number, string test)
+        {
+        }
+
+        private string End()
+        {
+        }
+    }
+}
